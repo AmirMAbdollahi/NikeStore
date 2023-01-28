35166d6d32a677caddbb6f442e0d6f33be2b0e9a
Amir Mohammad Abdollahi <a.m.abdollahi2001@gmail.com>
2023-01-28 21:59:30 +0330

    theme login and signup

--- app/src/main/java/com/example/niketest/feature/auth/AuthActivity.kt
+++ app/src/main/java/com/example/niketest/feature/auth/AuthActivity.kt
@@ -8,5 +8,10 @@ class AuthActivity : NikeActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        supportFragmentManager.beginTransaction().apply {
+           replace(R.id.fragmentContainer,SignUpFragment())
+        }.commit()
+
     }
 }
